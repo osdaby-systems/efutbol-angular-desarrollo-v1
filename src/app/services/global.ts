02513ad1c85e1d49d4aa4c77ef9b10fff9e34c5c
darwin <darwinj94@hotmail.com>
2017-10-13 15:06:19 -0500

    global

--- src/app/services/global.ts
+++ src/app/services/global.ts
@@ -1,6 +1,6 @@
 export var GLOBAL = {
-    // url: 'http://198.199.122.155:3999/api/'
-    url: 'http://localhost:3999/'
+     url: 'http://198.199.122.155:3999/api/'
+    // url: 'http://localhost:3999/'
 }
 
 //url: 'http://198.199.122.155:3999/api/'
